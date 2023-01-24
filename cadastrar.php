b2b840863300896f92c7be155fad518ef5b7508e
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-24 17:46:20 -0300

    ok

--- cadastrar.php
+++ cadastrar.php
@@ -25,4 +25,13 @@ if (isset($_POST['cadastrar_php'])) {
     $stmt->$conn->prepare(
         'INSERT INTO usuarios (first_name, last_name, password, email)VALUES(:first_name, :last_name, :password, :email)'
     );
+
+    $stmt->bindValue(':first_name', $first_name, PDO::PARAM_STR);
+    $stmt->bindValue(':last_name', $last_name, PDO::PARAM_STR);
+    $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+    if ($stmt->$conn->rowCount() > 0) {
+        echo 'Usuário cadastrado com sucesso';
+    }
 }
