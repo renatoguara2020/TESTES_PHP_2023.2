4367ae822ef5486ddd79340c8c4be65b58201d92
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-24 19:29:11 -0300

    ok

--- connection1.php
+++ connection1.php
@@ -12,7 +12,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    echo "<h3>Connected Successfully with Database: $database</h3>";
+    echo '<h3>Connected Successfully with Database</h3>';
 } catch (PDOException $e) {
     echo 'Error: ' . $e->getMessage() . '<br/> ' . $e->getTraceAsString();
 }
