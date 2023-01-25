1e35a930f88fa2bda0dd4ad87878114b0ec32ab5
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-24 21:17:40 -0300

    ok

--- cadastrar.php
+++ cadastrar.php
@@ -2,11 +2,6 @@
 
 include_once 'connection1.php';
 
-$first_name = filter_input(
-    INPUT_POST,
-    'first_name',
-    FILTER_SANITIZE_SPECIAL_CHARS
-);
 $last_name = filter_input(
     INPUT_POST,
     'last_name',
@@ -16,6 +11,14 @@ $last_name = filter_input(
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
 
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if (isset($_POST['first_name']) && $_POST['first_name'] != null) {
+    $first_name = filter_input(
+        INPUT_POST,
+        'first_name',
+        FILTER_SANITIZE_SPECIAL_CHARS
+    );
+}
 
 $stmt = $conn->prepare(
     'INSERT INTO usuarios(first_name, last_name, password, email, dataAtual)VALUES(:first_name, :last_name, :password, :email, NOW())'
